6aea632706457a857e21c06268a422b49877006a
TJ DeVries <tim.devries@dornerworks.com>
2016-09-13 12:51:49 -0400

    Fix several suggestions

--- rplugin/python3/denite/source/menu.py
+++ rplugin/python3/denite/source/menu.py
@@ -1,5 +1,5 @@
 # ============================================================================
-# FILE: line.py
+# FILE: menu.py
 # AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
 #         TJ DeVries <devries.timothyj at gmail.com>
 # License: MIT license
@@ -21,39 +21,46 @@ class Source(Base):
 
     def on_init(self, context):
         # TODO: Could also check for unite_source_menu_menus?
-        context['__menus'] = self.vim.vars['denite_source_menu_menus']
+        context['__menus'] = context.get('menus',
+                                         self.vim.vars.get(
+                                             'denite_source_menu_menus',
+                                             {}
+                                         ))
 
     def gather_candidates(self, context):
         lines = []
 
-        menu_context = [option for option in context['sources'] if option['name'] == self.name][0]
+        menu_context = [option for option in context['sources']
+                        if option['name'] == self.name][0]
 
         if menu_context == []:
             # Exit quickly
             return []
+
+        # TODO: Get all the arguments,
+        # in case some aren't passed directly to the menu
+        if menu_context['args']:
+            # TODO:  Check the menu name
+
+            # Handle file candidates
+            lines.extend([
+                {'word': str(candidate[0]),
+                 'action__path': candidate[1],
+                 }
+                for search_string in menu_context['args']
+                for candidate
+                in context['__menus'][search_string]['file_candidates']
+            ])
+
+            # TODO: Handle command candidates
+            # TODO: Handle candidates
         else:
-            # TODO: Get all the arguments, in case some aren't passed directly to the menu
-            if menu_context['args']:
-                # TODO:  Check the menu name
-
-                # Handle file candidates
-                lines.extend([{'word': str(candidate[0]),
-                                'action__path': candidate[1], 
-                                }
-                                for search_string in menu_context['args']
-                                for candidate in context['__menus'][search_string]['file_candidates']
-                                ])
-
-                # TODO: Handle command candidates
-                # TODO: Handle candidates
-            else:
-                # TODO: Display all the available menus
-                menu_options = context['__menus']
-
-                lines.extend([{'word': candidate,
-                               # TODO: Open the menu?
-                               }
-                              for candidate in context['__menus']]
-                             )
+            # TODO: Display all the available menus
+            lines.extend([{'word': candidate,
+                           # TODO: Open the menu?
+                           }
+                          for candidate in context['__menus']
+                          ]
+                         )
 
         return lines
