d22b07f9f10e111b5c24ef2c8ba09862b99c440a
TJ DeVries <devries.timothyj@gmail.com>
2016-09-17 20:37:53 -0400

    Use `self.vars`

--- rplugin/python3/denite/source/menu.py
+++ rplugin/python3/denite/source/menu.py
@@ -19,50 +19,53 @@ class Source(Base):
         # self.matchers = []
         # self.sorters = []
 
-        self.dict_key = '__menus'
+        self.vars = {
+            'menus': {}
+        }
 
     def on_init(self, context):
-        context[self.dict_key] = context['custom']['source']['menu']['vars']\
-            .get('menus', {})
-
         # TODO: Set a value to look for old unite menus from VIM?
         unite_menu_compatibilty = False
         if unite_menu_compatibilty:
-            context[self.dict_key].update(
+            self.vars['menus'].update(
                 self.vim.vars['unite_source_menu_menus']
             )
 
     def gather_candidates(self, context):
         # If no menus have been defined, just exit
-        if self.dict_key not in context.keys() or not context[self.dict_key]:
+        if 'menus' not in self.vars.keys() or self.vars['menus'] == {}:
             return []
 
         lines = []
+        menus = self.vars['menus']
+        args = context['args']
 
-        menu_args = context['args']
+        if args:
+            # Loop through each menu option
+            for menu in args:
+                # If a menu doesn't exist, just continue gracefully
+                # TODO: Print an error letting the user know
+                if menu not in menus.keys():
+                    continue
 
-        if menu_args:
-            # TODO:  Check the menu name
+                # Handle file candidates
+                if 'file_candidates' in menus[menu]:
+                    lines.extend([
+                        {'word': str(candidate[0]),
+                         'action__path': candidate[1],
+                         }
+                        for candidate in menus[menu]['file_candidates']
+                    ])
 
-            # Handle file candidates
-            lines.extend([
-                {'word': str(candidate[0]),
-                 'action__path': candidate[1],
-                 }
-                for search_string in menu_args
-                for candidate
-                in context[self.dict_key][search_string]['file_candidates']
-            ])
-
-            # TODO: Handle command candidates
-            # TODO: Handle candidates
+                # TODO: Handle command candidates
+                # TODO: Handle candidates
         else:
             # TODO: Display all the available menus
-            lines.extend([{'word': candidate,
-                           # TODO: Open the menu?
-                           }
-                          for candidate in context[self.dict_key]
-                          ]
-                         )
+            lines.extend([
+                {'word': candidate,
+                 # TODO: Open the menu?
+                 }
+                for candidate in menus
+            ])
 
         return lines
