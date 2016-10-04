e7455681eac66dd2a313524b6ecc8af0969968c8
okamos <himinato.k@gmail.com>
2016-10-04 18:13:39 +0900

    Add directory kind

---
+++ rplugin/python3/denite/kind/directory.py
@@ -0,0 +1,21 @@
+# ============================================================================
+# FILE: directory.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+#         okamos <himinato.k at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from .base import Base
+
+
+class Kind(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'directory'
+
+    def action_default(self, context):
+        target = context['targets'][0]
+        # TODO want to narrow
+        self.vim.command('cd {}'.format(target['action__path']))
