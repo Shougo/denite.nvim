4e1d7dbec6e4fee9a9033dd9e88dec405afcfe38
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-10-06 23:13:24 +0900

    Add converter_relative_word

---
+++ rplugin/python3/denite/filter/converter_relative_word.py
@@ -0,0 +1,23 @@
+# ============================================================================
+# FILE: converter_relative_word.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from .base import Base
+from os.path import relpath
+
+
+class Filter(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'converter_relative_word'
+        self.description = 'convert candidate word to relative path'
+
+    def filter(self, context):
+        for candidate in context['candidates']:
+            candidate['word'] = relpath(
+                candidate['word'], start=context['directory'])
+        return context['candidates']
