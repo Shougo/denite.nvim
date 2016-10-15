573add6f8cfbf6e28385e7ef5f61a12d7f2c7373
Herrington Darkholme <nonamesheep1@gmail.com>
2016-10-15 18:53:59 +0800

    Fix #13, implement incremental output

--- rplugin/python3/denite/process.py
+++ rplugin/python3/denite/process.py
@@ -5,6 +5,10 @@
 # ============================================================================
 
 import subprocess
+from threading import Thread
+from queue import Queue
+from time import time
+from collections import deque
 
 
 class Process(object):
@@ -15,6 +19,9 @@ class Process(object):
                                        cwd=cwd)
         self.__eof = False
         self.__context = context
+        self.__thread = Thread(target=self.enqueue_output)
+        self.__thread.start()
+        self.__queue = Queue()
 
     def eof(self):
         return self.__eof
@@ -24,22 +31,35 @@ class Process(object):
             return
         self.__proc.kill()
         self.__proc = None
+        self.__queue = None
+        self.__thread = None
+
+    def enqueue_output(self):
+        for line in self.__proc.stdout:
+            self.__queue.put(line.decode(self.__context['encoding'],
+                             errors='replace').strip('\n'))
 
     def communicate(self, timeout):
         if not self.__proc:
             return []
 
-        outs = []
-        errs = []
-        try:
-            outs, errs = self.__proc.communicate(timeout=timeout)
-            outs = outs.decode(self.__context['encoding'],
-                               errors='replace').split('\n')
-            errs = errs.decode(self.__context['encoding'],
-                               errors='replace').split('\n')
-            self.__eof = True
-        except subprocess.TimeoutExpired:
-            return (outs, errs)
+        start = time()
+        outs = deque()
 
+        while not self.__queue.empty() and time() < start + timeout:
+            outs.append(self.__queue.get_nowait())
+
+        outs = list(outs)
+
+        if self.__thread.is_alive() or not self.__queue.empty():
+            return (outs, [])
+
+        _, errs = self.__proc.communicate(timeout=timeout)
+        errs = errs.decode(self.__context['encoding'],
+                           errors='replace').split('\n')
+        self.__eof = True
         self.__proc = None
+        self.__thread = None
+        self.__queue = None
+
         return (outs, errs)
