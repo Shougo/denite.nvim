0bea3e9deddb19e9b998aa673099d03acf40ccd3
lambdalisue <lambdalisue@hashnote.net>
2016-12-19 19:14:54 +0900

    Forgot to add. 'assert' is required for pytest.

--- test/rplugin/python3/denite/test_util.py
+++ test/rplugin/python3/denite/test_util.py
@@ -2,10 +2,10 @@ import denite.util as util
 
 
 def test_convert2fuzzy_pattern():
-    util.convert2fuzzy_pattern('abc') == 'a[^a]*b[^b]*c'
-    util.convert2fuzzy_pattern('a/c') == 'a[^a]*/[^/]*c'
+    assert util.convert2fuzzy_pattern('abc') == 'a[^a]*b[^b]*c'
+    assert util.convert2fuzzy_pattern('a/c') == 'a[^a]*/[^/]*c'
 
 
 def test_convert2regex_pattern():
-    util.convert2regex_pattern('def') == 'def'
-    util.convert2regex_pattern('foo bar') == 'foo\|bar'
+    assert util.convert2regex_pattern('def') == 'def'
+    assert util.convert2regex_pattern('foo bar') == 'foo\|bar'
