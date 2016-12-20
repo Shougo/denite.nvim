33969fcb1fb13f7bfdca773f35d5849d69c6aa87
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-12-20 20:58:50 +0900

    Add `jump` action to jump opened buffer window

--- rplugin/python3/denite/kind/buffer.py
+++ rplugin/python3/denite/kind/buffer.py
@@ -18,3 +18,11 @@ class Kind(Openable):
     def action_open(self, context):
         for target in context['targets']:
             self.vim.command('buffer {0}'.format(target['action__bufnr']))
+
+    def action_jump(self, context):
+        for target in context['targets']:
+            winids = self.vim.call('win_findbuf', target['action__bufnr'])
+            if len(winids) == 0:
+                self.action_tabopen(context)
+            else:
+                self.vim.call('win_gotoid', winids[0])
