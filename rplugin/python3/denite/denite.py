03a7960bc78437905bb9a03525e621d2e97dba6b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-01-24 10:29:07 +0900

    Use _ instead of __

--- rplugin/python3/denite/denite.py
+++ rplugin/python3/denite/denite.py
@@ -17,41 +17,41 @@ import copy
 class Denite(object):
 
     def __init__(self, vim):
-        self.__vim = vim
-        self.__sources = {}
-        self.__filters = {}
-        self.__kinds = {}
-        self.__runtimepath = ''
-        self.__current_sources = []
+        self._vim = vim
+        self._sources = {}
+        self._filters = {}
+        self._kinds = {}
+        self._runtimepath = ''
+        self._current_sources = []
 
     def start(self, context):
-        self.__custom = context['custom']
+        self._custom = context['custom']
 
-        if self.__vim.options['runtimepath'] != self.__runtimepath:
+        if self._vim.options['runtimepath'] != self._runtimepath:
             # Recache
             self.load_sources(context)
             self.load_filters(context)
             self.load_kinds(context)
-            self.__runtimepath = self.__vim.options['runtimepath']
+            self._runtimepath = self._vim.options['runtimepath']
 
         for alias, base in [[x, y] for [x, y] in
-                            self.__custom['alias_source'].items()
-                            if x not in self.__sources]:
-            if base not in self.__sources:
+                            self._custom['alias_source'].items()
+                            if x not in self._sources]:
+            if base not in self._sources:
                 self.error('Invalid base: ' + base)
                 continue
-            self.__sources[alias] = copy.copy(self.__sources[base])
-            self.__sources[alias].name = alias
+            self._sources[alias] = copy.copy(self._sources[base])
+            self._sources[alias].name = alias
 
     def gather_candidates(self, context):
-        for source in self.__current_sources:
+        for source in self._current_sources:
             source.context['is_redraw'] = context['is_redraw']
             candidates = source.gather_candidates(source.context)
             source.context['all_candidates'] = candidates
             source.context['candidates'] = candidates
 
     def filter_candidates(self, context):
-        for source in self.__current_sources:
+        for source in self._current_sources:
             ctx = source.context
             ctx['input'] = context['input']
             ctx['mode'] = context['mode']
@@ -64,17 +64,17 @@ class Denite(object):
             partial = []
             for i in range(0, len(entire), 1000):
                 ctx['candidates'] = entire[i:i+1000]
-                for matcher in [self.__filters[x]
+                for matcher in [self._filters[x]
                                 for x in source.matchers
-                                if x in self.__filters]:
+                                if x in self._filters]:
                     ctx['candidates'] = matcher.filter(ctx)
                 partial += ctx['candidates']
                 if len(partial) >= 1000:
                     break
             ctx['candidates'] = partial
-            for f in [self.__filters[x]
+            for f in [self._filters[x]
                       for x in source.sorters + source.converters
-                      if x in self.__filters]:
+                      if x in self._filters]:
                 ctx['candidates'] = f.filter(ctx)
             partial = ctx['candidates']
             for c in partial:
@@ -83,13 +83,13 @@ class Denite(object):
             yield source.name, entire, partial
 
     def on_init(self, context):
-        self.__current_sources = []
+        self._current_sources = []
         for [name, args] in [[x['name'], x['args']]
                              for x in context['sources']]:
-            if name not in self.__sources:
+            if name not in self._sources:
                 self.error('Source "' + name + '" is not found.')
                 continue
-            source = self.__sources[name]
+            source = self._sources[name]
             source.context = copy.deepcopy(context)
             source.context['args'] = args
             source.context['is_async'] = False
@@ -98,99 +98,99 @@ class Denite(object):
 
             # Set the source attributes.
             source.matchers = get_custom_source(
-                self.__custom, source.name,
+                self._custom, source.name,
                 'matchers', source.matchers)
             source.sorters = get_custom_source(
-                self.__custom, source.name,
+                self._custom, source.name,
                 'sorters', source.sorters)
             source.converters = get_custom_source(
-                self.__custom, source.name,
+                self._custom, source.name,
                 'converters', source.converters)
             source.vars.update(
-                get_custom_source(self.__custom, source.name,
+                get_custom_source(self._custom, source.name,
                                   'vars', source.vars))
             if not source.context['args']:
                 source.context['args'] = get_custom_source(
-                    self.__custom, source.name, 'args', [])
+                    self._custom, source.name, 'args', [])
 
             if hasattr(source, 'on_init'):
                 source.on_init(source.context)
-            self.__current_sources.append(source)
+            self._current_sources.append(source)
 
-        for filter in [x for x in self.__filters.values()
-                       if x.vars and x.name in self.__custom['filter']]:
-            filter.vars.update(self.__custom['filter'][filter.name])
+        for filter in [x for x in self._filters.values()
+                       if x.vars and x.name in self._custom['filter']]:
+            filter.vars.update(self._custom['filter'][filter.name])
 
     def on_close(self, context):
-        for source in self.__current_sources:
+        for source in self._current_sources:
             if hasattr(source, 'on_close'):
                 source.on_close(source.context)
 
     def debug(self, expr):
-        denite.util.debug(self.__vim, expr)
+        denite.util.debug(self._vim, expr)
 
     def error(self, msg):
-        self.__vim.call('denite#util#print_error', msg)
+        self._vim.call('denite#util#print_error', msg)
 
     def get_sources(self):
-        return self.__sources
+        return self._sources
 
     def get_source(self, name):
-        return self.__sources.get(name, {})
+        return self._sources.get(name, {})
 
     def get_current_sources(self):
-        return self.__current_sources
+        return self._current_sources
 
     def load_sources(self, context):
         # Load sources from runtimepath
-        loaded_paths = [x.path for x in self.__sources.values()]
+        loaded_paths = [x.path for x in self._sources.values()]
         for path, name in find_rplugins(context, 'source', loaded_paths):
             module = importlib.machinery.SourceFileLoader(
                 'denite.source.' + name, path).load_module()
-            source = module.Source(self.__vim)
-            self.__sources[source.name] = source
+            source = module.Source(self._vim)
+            self._sources[source.name] = source
             source.path = path
             syntax_name = 'deniteSource_' + source.name.replace('/', '_')
             if not source.syntax_name:
                 source.syntax_name = syntax_name
 
-            if source.name in self.__custom['alias_source']:
+            if source.name in self._custom['alias_source']:
                 # Load alias
-                for alias in self.__custom['alias_source'][source.name]:
-                    self.__sources[alias] = module.Source(self.__vim)
-                    self.__sources[alias].name = alias
-                    self.__sources[alias].path = path
-                    self.__sources[alias].syntax_name = syntax_name
+                for alias in self._custom['alias_source'][source.name]:
+                    self._sources[alias] = module.Source(self._vim)
+                    self._sources[alias].name = alias
+                    self._sources[alias].path = path
+                    self._sources[alias].syntax_name = syntax_name
 
     def get_filter(self, filter_name):
-        return self.__filters.get(filter_name, None)
+        return self._filters.get(filter_name, None)
 
     def load_filters(self, context):
         # Load filters from runtimepath
-        loaded_paths = [x.path for x in self.__filters.values()]
+        loaded_paths = [x.path for x in self._filters.values()]
         for path, name in find_rplugins(context, 'filter', loaded_paths):
             module = importlib.machinery.SourceFileLoader(
                 'denite.filter.' + name, path).load_module()
-            f = module.Filter(self.__vim)
+            f = module.Filter(self._vim)
             f.path = path
-            self.__filters[name] = f
+            self._filters[name] = f
 
-            if name in self.__custom['alias_filter']:
+            if name in self._custom['alias_filter']:
                 # Load alias
-                for alias in self.__custom['alias_filter'][name]:
-                    self.__filters[alias] = module.Filter(self.__vim)
-                    self.__filters[alias].name = alias
-                    self.__filters[alias].path = path
+                for alias in self._custom['alias_filter'][name]:
+                    self._filters[alias] = module.Filter(self._vim)
+                    self._filters[alias].name = alias
+                    self._filters[alias].path = path
 
     def load_kinds(self, context):
         # Load kinds from runtimepath
-        loaded_paths = [x.path for x in self.__kinds.values()]
+        loaded_paths = [x.path for x in self._kinds.values()]
         for path, name in find_rplugins(context, 'kind', loaded_paths):
             module = importlib.machinery.SourceFileLoader(
                 'denite.kind.' + name, path).load_module()
-            kind = module.Kind(self.__vim)
+            kind = module.Kind(self._vim)
             kind.path = path
-            self.__kinds[name] = kind
+            self._kinds[name] = kind
 
     def do_action(self, context, action_name, targets):
         action = self.get_action(context, action_name, targets)
@@ -207,7 +207,7 @@ class Denite(object):
         kinds = set()
         for target in targets:
             k = target['kind'] if 'kind' in target else (
-                self.__sources[target['source']].kind)
+                self._sources[target['source']].kind)
             kinds.add(k)
         if len(kinds) != 1:
             self.error('Multiple kinds are detected')
@@ -217,11 +217,11 @@ class Denite(object):
 
         if isinstance(k, str):
             # k is kind name
-            if k not in self.__kinds:
+            if k not in self._kinds:
                 self.error('Invalid kind: ' + k)
                 return {}
 
-            kind = self.__kinds[k]
+            kind = self._kinds[k]
         else:
             kind = k
 
@@ -255,5 +255,5 @@ class Denite(object):
                               if x.find('action_') == 0]
 
     def is_async(self):
-        return len([x for x in self.__current_sources
+        return len([x for x in self._current_sources
                     if x.context['is_async']]) > 0
