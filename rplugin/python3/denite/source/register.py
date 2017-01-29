93c3576e59d2a54e5628f31162dd3b5173b4a509
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-01-29 13:11:21 +0900

    Fix register source

--- rplugin/python3/denite/source/register.py
+++ rplugin/python3/denite/source/register.py
@@ -19,15 +19,14 @@ class Source(Base):
     def gather_candidates(self, context):
         candidates = []
 
-        for reg in [
+        for reg in (['+', '*']
+                    if self.vim.call('has', 'clipboard') else []) + [
            '"',
            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
            'u', 'v', 'w', 'x', 'y', 'z',
-           '-', '.', ':', '#', '%', '/', '=',
-        ] + (['+', '*'] if self.vim.call('has', 'clipboard') else []):
-
+           '-', '.', ':', '#', '%', '/', '=']:
             register = self.vim.call('getreg', reg, 1)
             if register:
                 candidates.append({
