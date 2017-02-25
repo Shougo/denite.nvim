15879a1e8d5fbceef3277b144341157030278cd2
skeept <skeept@gmail.com>
2017-02-25 13:25:20 -0600

    add license; use 2 branches to define scantree

--- rplugin/python3/denite/scantree.py
+++ rplugin/python3/denite/scantree.py
@@ -1,29 +1,47 @@
+# ============================================================================
+# FILE: scantree.py
+# AUTHOR: Jorge Rodrigues <skeept at gmail.com>
+# License: MIT license
+# ============================================================================
+
 import sys
-import os
+from os.path import basename, join
+from os import walk
 
+try:
+    from os import scandir2
+    def scantree(path_name, skip_list=None):
+        """if scandir is available use it as it is faster than os.walk"""
 
-def scantree(pathn):
-    """Use scandir if available else os.walk.
-    scandir is supposed to be much faster"""
+        if skip_list is None:
+            skip_list = ['.git', '.hg']
 
-    skip_list = ['.git']
-
-    if 'scandir' in dir(os):
-        for entry in os.scandir(pathn):
+        for entry in scandir(path_name):
             if entry.is_dir(follow_symlinks=False):
-                if os.path.basename(entry.path) not in skip_list:
+                if basename(entry.path) not in skip_list:
                     yield from scantree(entry.path)
             else:
                 yield entry.path
-    else:
-        for root, dirn, files in os.walk(pathn):
-            if os.path.basename(root) in skip_list:
+except ImportError:
+    def scantree(path_name, skip_list=None):
+        """scandir is not available use os.walk"""
+
+        if skip_list is None:
+            skip_list = ['.git', '.hg']
+
+        for root, dirn, files in walk(path_name):
+            if basename(root) in skip_list:
                 continue
             for fn in files:
-                yield os.path.join(root, fn)
+                yield join(root, fn)
+
+
+def output_files():
+    """print the list of files to stdout"""
+    for path_name in sys.argv[1:]:
+        for fn in scantree(path_name):
+            sys.stdout.write(fn + '\n')
 
 
 if __name__ == "__main__":
-    for pathn in sys.argv[1:]:
-        for fn in scantree(pathn):
-            sys.stdout.write(fn + '\n')
+    output_files()
