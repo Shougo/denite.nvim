04ad968488b7ac7ff05d99f81aa781ae4891c111
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-04-30 15:23:32 +0900

    Fix #260 guicursor problem

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -459,6 +459,7 @@ class Default(object):
         self._vim.command('highlight! link CursorLine CursorLine')
         if self._vim.call('exists', '#ColorScheme'):
             self._vim.command('silent doautocmd ColorScheme')
+        self._vim.command('set guicursor&')
         self._vim.options['guicursor'] = self._guicursor
 
     def quit_buffer(self):
