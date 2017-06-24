12ec47e1f57dbcdbd3a590ee328a69f6cb136966
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-06-24 16:31:56 +0900

    Add direction feature in denite.

--- rplugin/python3/denite/source/line.py
+++ rplugin/python3/denite/source/line.py
@@ -27,19 +27,31 @@ class Source(Base):
         context['__linenr'] = self.vim.current.window.cursor[0]
         context['__bufname'] = self.vim.current.buffer.name
         context['__bufnr'] = self.vim.current.buffer.number
+        context['__direction'] = 'all'
+        context['__fmt'] = '%' + str(len(
+            str(self.vim.call('line', '$')))) + 'd: %s'
+        if context['args'] and (context['args'][0] == 'all' or
+                                context['args'][0] == 'forward' or
+                                context['args'][0] == 'backward'):
+            context['__direction'] = context['args'][0]
 
     def highlight(self):
         self.vim.command(LINE_NUMBER_SYNTAX + self.syntax_name)
         self.vim.command(LINE_NUMBER_HIGHLIGHT)
 
     def gather_candidates(self, context):
-        fmt = '%' + str(len(str(self.vim.call('line', '$')))) + 'd: %s'
-
+        linenr = context['__linenr']
         lines = [{'word': x,
-                  'abbr': (fmt % (i + 1, x)),
+                  'abbr': (context['__fmt'] % (i + 1, x)),
                   'action__path': context['__bufname'],
                   'action__line': (i + 1)}
                  for [i, x] in
                  enumerate(self.vim.call(
                      'getbufline', context['__bufnr'], 1, '$'))]
-        return lines[context['__linenr']-1:] + lines[:context['__linenr']-1]
+        if context['__direction'] == 'all':
+            return lines
+        elif context['__direction'] == 'backward':
+            return list(reversed(lines[:linenr])) + list(
+                reversed(lines[linenr:]))
+        else:
+            return lines[linenr-1:] + lines[:linenr-1]
