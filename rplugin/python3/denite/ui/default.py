ff3016ddf22bfd82768c10726b961c524f19754a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-07-19 09:17:41 +0900

    Fix #320 fold problem

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -506,6 +506,7 @@ class Default(object):
         self._vim.command('highlight! link CursorLine CursorLine')
         if self._vim.call('exists', '#ColorScheme'):
             self._vim.command('silent doautocmd ColorScheme')
+            self._vim.command('normal! zv')
         if self._context['cursor_shape']:
             self._vim.command('set guicursor&')
             self._vim.options['guicursor'] = self._guicursor
