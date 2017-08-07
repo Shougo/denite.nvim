d4348729315a489104d340295fa10b17ad69bd7e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-08-08 08:51:55 +0900

    Add converter_relative_abbr

---
+++ rplugin/python3/denite/filter/converter_relative_abbr.py
@@ -0,0 +1,24 @@
+# ============================================================================
+# FILE: converter_relative_abbr.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from .base import Base
+from os.path import relpath, isabs
+
+
+class Filter(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'converter_relative_abbr'
+        self.description = 'convert candidate abbr to relative path'
+
+    def filter(self, context):
+        for candidate in context['candidates']:
+            if isabs(candidate['word']):
+                candidate['abbr'] = relpath(
+                    candidate['word'], start=context['path'])
+        return context['candidates']
