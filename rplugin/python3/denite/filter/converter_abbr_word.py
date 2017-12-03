840f57805056779a05035c67392c8faced6f37dc
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-12-03 13:09:28 +0900

    Add converter_abbr_word

---
+++ rplugin/python3/denite/filter/converter_abbr_word.py
@@ -0,0 +1,22 @@
+# ============================================================================
+# FILE: converter_abbr_word.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from .base import Base
+
+
+class Filter(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = 'converter_abbr_word'
+        self.description = 'convert candidate abbr to word'
+
+    def filter(self, context):
+        for candidate in context['candidates']:
+            if 'abbr' in candidate:
+                candidate['word'] = candidate['abbr']
+        return context['candidates']
