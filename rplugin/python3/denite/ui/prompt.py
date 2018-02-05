abe181fb76298f52d039216623eb9602575b2b0f
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-02-06 06:35:33 +0900

    Change harvest_interval default value

--- rplugin/python3/denite/ui/prompt.py
+++ rplugin/python3/denite/ui/prompt.py
@@ -18,10 +18,6 @@ class DenitePrompt(Prompt):
         self.action.unregister('prompt:accept', fail_silently=True)
         self.action.unregister('prompt:cancel', fail_silently=True)
 
-        self.harvest_interval = 0.01
-        self._previous_text = self.text
-        self._timeout = datetime.now()
-
     @property
     def text(self):
         return self.context.get('input', '')
@@ -50,7 +46,9 @@ class DenitePrompt(Prompt):
         # NOTE:
         # 'inputsave' is not required to be called while denite call it
         # at denite#start
-        pass
+        self.harvest_interval = 0.03
+        self._previous_text = self.text
+        self._timeout = datetime.now()
 
     def on_term(self, status):
         # NOTE:
