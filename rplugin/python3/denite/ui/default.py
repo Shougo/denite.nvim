ca23187e4e314e0adec8486dc3e1ea7e148ab1cb
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-02-22 22:54:13 +0900

    Fix #451 blink problem

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -257,7 +257,6 @@ class Default(object):
 
     def init_syntax(self):
         self._vim.command('syntax case ignore')
-        self._vim.command('highlight clear')
         self._vim.command('highlight default link deniteMode ModeMsg')
         self._vim.command('highlight link deniteMatchedRange ' +
                           self._context['highlight_matched_range'])
