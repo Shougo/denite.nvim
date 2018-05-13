ca124f4dcd90c918b2e3ac50e6929ae72b68a193
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-05-13 14:13:39 +0900

    Fix #479 range support in command kind

--- rplugin/python3/denite/kind/command.py
+++ rplugin/python3/denite/kind/command.py
@@ -18,7 +18,8 @@ class Kind(Base):
 
     def action_execute(self, context):
         target = context['targets'][0]
-        self._execute(target['action__command'],
+        self._execute(context,
+                      target['action__command'],
                       target.get('action__is_pause', False))
 
     def action_edit(self, context):
@@ -27,12 +28,15 @@ class Kind(Base):
                              "command > ",
                              target['action__command'],
                              'command')
-        self._execute(command,
+        self._execute(context, command,
                       target.get('action__is_pause', False))
 
-    def _execute(self, command, is_pause):
+    def _execute(self, context, command, is_pause):
         if not command:
             return
+        if context['firstline'] != context['lastline']:
+            command = '{},{}{}'.format(
+                context['firstline'], context['lastline'], command)
         output = self.vim.call(
             'denite#util#execute_command', command, is_pause)
         if not output or output == '\n':
