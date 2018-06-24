d408e23befe6cd0960850e5a2f937109102fc9fb
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-06-24 12:12:40 +0900

    Improve register source

--- rplugin/python3/denite/source/register.py
+++ rplugin/python3/denite/source/register.py
@@ -28,10 +28,12 @@ class Source(Base):
            'u', 'v', 'w', 'x', 'y', 'z',
            '-', '.', ':', '#', '%', '/', '=']:
             register = self.vim.call('getreg', reg, 1)
-            if register:
-                candidates.append({
-                    'word': reg + ': ' + re.sub(
-                        r'\n', r'\\n', register)[:200],
-                    'action__text': register,
-                })
+            if not register:
+                continue
+
+            candidates.append({
+                'word': reg + ': ' + re.sub(
+                    r'\n', r'\\n', register)[:200],
+                'action__text': register,
+            })
         return candidates
