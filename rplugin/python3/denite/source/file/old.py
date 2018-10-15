2f2c2883100d71191066e10ca236324dff397a02
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-10-15 22:06:58 +0900

    Improve file/old check

--- rplugin/python3/denite/source/file/old.py
+++ rplugin/python3/denite/source/file/old.py
@@ -23,9 +23,11 @@ class Source(Base):
             self.vim.command('wviminfo | rviminfo!')
 
     def gather_candidates(self, context):
-        return [{'word': x, 'action__path': x}
-                for x in [expand(x) for x in
-                          self.vim.call('denite#helper#_get_oldfiles')]]
+        oldfiles = [
+            expand(x) for x in self.vim.call('denite#helper#_get_oldfiles')
+            if not self.vim.call('bufexists', x) or
+            self.vim.call('getbufvar', x, 'buftype') == '']
+        return [{'word': x, 'action__path': x} for x in oldfiles]
 
 
 class Kind(File):
