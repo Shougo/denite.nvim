fc5fc1013b2f0cda1778c00be1d2850040d0a01d
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-11-18 17:40:46 +0900

    Fix flake8 errors

--- rplugin/python3/denite/source/colorscheme.py
+++ rplugin/python3/denite/source/colorscheme.py
@@ -7,6 +7,8 @@
 from os import path
 
 from denite.source.base import Base
+from denite.kind.command import Kind as Command
+
 from denite.util import globruntime
 
 
@@ -16,14 +18,14 @@ class Source(Base):
         super().__init__(vim)
 
         self.name = 'colorscheme'
-        self.kind = 'colorscheme'
+        self.kind = Kind(vim)
 
     def on_init(self, context):
         context['currentColor'] = self.vim.vars['colors_name']
 
-
     def on_close(self, context):
-        self.vim.command('silent colorscheme {}'.format(context['currentColor']))
+        self.vim.command('silent colorscheme {}'.format(
+            context['currentColor']))
 
     def gather_candidates(self, context):
         colorschemes = {}
@@ -36,3 +38,14 @@ class Source(Base):
             }
 
         return sorted(colorschemes.values(), key=lambda value: value['word'])
+
+
+class Kind(Command):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.vim = vim
+        self.name = 'colorscheme'
+
+    def action_preview(self, context):
+        target = context['targets'][0]
+        self.vim.command('silent colorscheme {}'.format(target['word']))
