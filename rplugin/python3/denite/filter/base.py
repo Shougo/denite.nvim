0dfc2a4175d77837c272c62d91889fd1c02fd833
Shougo Matsushita <Shougo.Matsu@gmail.com>
2019-03-12 09:10:08 +0900

    Add base files

--- rplugin/python3/denite/filter/base.py
+++ rplugin/python3/denite/filter/base.py
@@ -4,36 +4,10 @@
 # License: MIT license
 # ============================================================================
 
-from abc import abstractmethod
-import denite.util
+# For backward compatibility
+from denite.base.filter import Base as _Base
 
 
-class Base(object):
-
+class Base(_Base):
     def __init__(self, vim):
-        self.vim = vim
-        self.name = 'base'
-        self.description = ''
-        self.vars = {}
-
-    @abstractmethod
-    def filter(self, context):
-        pass
-
-    def convert_pattern(self, input_str):
-        return ''
-
-    def debug(self, expr):
-        denite.util.debug(self.vim, expr)
-
-    def print_message(self, context, expr):
-        context['messages'].append(self.name + ': ' + str(expr))
-
-    def error_message(self, context, expr):
-        prefix = self.name + ': '
-        if isinstance(expr, list):
-            for line in expr:
-                denite.util.error(self.vim, prefix + line)
-        else:
-            denite.util.error(self.vim, prefix + str(expr))
-        context['error_messages'].append(prefix + str(expr))
+        super().__init__(vim)
