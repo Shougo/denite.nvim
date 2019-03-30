3ccc143f81bab081b7495eb3d620ebc0b7a75ea6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2019-03-30 19:18:21 +0900

    Improve mark

--- rplugin/python3/denite/source/mark.py
+++ rplugin/python3/denite/source/mark.py
@@ -53,7 +53,8 @@ class Source(Base):
             if self.empty_mark(mark_info):
                 continue
 
-            bufname = self.vim.call('bufname', bufnum if bufnum != 0 else "%")
+            bufname = self.vim.call('bufname',
+                                    bufnum if bufnum != 0 else '%')
             path = self.vim.call('fnamemodify', bufname, ':p')
             if bufnum == 0:
                 file_or_text = 'text: ' + self.vim.call('getline', lnum)
