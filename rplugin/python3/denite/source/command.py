3d34f9db5f82669f919395480e614654b80e8cf8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2020-10-10 19:27:30 +0900

    Improve file source

--- rplugin/python3/denite/source/command.py
+++ rplugin/python3/denite/source/command.py
@@ -19,25 +19,15 @@ class Source(Base):
 
         self.name = 'command'
         self.kind = Kind(vim)
-        self.commands: Candidates = []
-
-        self._re_command = re.compile(r'^\|:.+\|')
-        self._re_tokens = re.compile(
-            r'^\|:(.+)\|[\t\s]+:([^\t]+)[\t\s]+(.+)')
-
-    def on_init(self, context: UserContext) -> None:
-        runtimepath = self.vim.eval('&runtimepath')
-        self._helpfiles = globruntime(runtimepath, 'doc/index.txt')
-        self._commands = globruntime(runtimepath, 'doc/index.txt')
 
     def gather_candidates(self, context: UserContext) -> Candidates:
         context['is_interactive'] = True
 
         has_cmdline = self.vim.call('denite#helper#has_cmdline')
+        if not has_cmdline:
+            return []
         if ' ' not in context['input'] or not has_cmdline:
-            if not self.commands:
-                self._init_commands()
-            return self.commands + [{
+            return [{
                 'action__command': x,
                 'word': x,
             } for x in self.vim.call('getcompletion', '', 'command')]
@@ -57,23 +47,6 @@ class Source(Base):
                 'action__histadd': True,
             }]
         return candidates
-
-    def _init_commands(self) -> None:
-        for helpfile in self._helpfiles:
-            with open(helpfile) as doc:
-                for line in [x for x in doc.readlines()
-                             if self._re_command.match(x)]:
-                    m = self._re_tokens.match(line)
-                    if not m:
-                        continue
-                    tokens = m.groups()
-                    command = f"execute input(':{tokens[0]} ')"
-                    self.commands.append({
-                        'action__command': command,
-                        'word': '{0:<20} -- {1}'.format(
-                            tokens[0], tokens[2],
-                        ),
-                    })
 
 
 class Kind(Command):
