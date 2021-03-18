79319b4cc0bff1a0e47f3401e35f93da961acf47
matsui54 <haru.matu9168@gmail.com>
2021-03-18 16:04:42 +0900

    Open help in help window

--- rplugin/python3/denite/source/help.py
+++ rplugin/python3/denite/source/help.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from pynvim import Nvim
 
 from denite.base.source import Base
+from denite.kind.file import Kind as File
 from denite.util import globruntime, UserContext, Candidates
 
 
@@ -18,7 +19,7 @@ class Source(Base):
         super().__init__(vim)
         self.vim = vim
         self.name = 'help'
-        self.kind = 'file'
+        self.kind = Kind(vim)
 
     def gather_candidates(self, context: UserContext) -> Candidates:
         candidates: Candidates = []
@@ -36,3 +37,23 @@ class Source(Base):
                     ),
                 }, ins)))
         return candidates
+
+
+class Kind(File):
+
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+        self.vim = vim
+        self.name = 'help'
+
+    def action_open(self, context: UserContext) -> None:
+        target = context['targets'][0]
+        self.vim.command(f'silent help {target["word"]}')
+
+    def action_vsplit(self, context: UserContext) -> None:
+        target = context['targets'][0]
+        self.vim.command(f'silent vertical help {target["word"]}')
+
+    def action_tabopen(self, context: UserContext) -> None:
+        for target in context['targets']:
+            self.vim.command(f'silent tab help {target["word"]}')
