1ef11c0431b202954727d35a92f6d436f9c4db8b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-05-15 17:53:25 +0900

    Fix grep path convert

--- rplugin/python3/denite/source/grep.py
+++ rplugin/python3/denite/source/grep.py
@@ -158,15 +158,15 @@ class Source(Base):
                 continue
             path = result[0]
             for searching_path in context['__paths']:
-                if (path == searching_path and
-                        not Path(searching_path).is_dir() and
-                        path.startswith(context['path'] + sep)):
+                if path == searching_path:
+                    continue
+
+                if path.startswith(context['path'] + sep):
                     # relative to context path
                     path = str(Path(path).relative_to(context['path']))
                     break
-                elif (path != searching_path and
-                        path.startswith(searching_path + sep)):
-                    # relative to searching_path
+                elif path.startswith(searching_path + sep):
+                    # relative to parent searching_path
                     path = str(Path(path).relative_to(
                         Path(searching_path).parent))
                     break
